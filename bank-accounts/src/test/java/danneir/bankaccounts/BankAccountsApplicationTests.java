7bd200a2be77c6e5f3892b778d18643b2d89c408
Daniel Develop <57878266+iath-dev@users.noreply.github.com>
2023-08-30 18:36:07 -0500

    Initial commit

---
+++ bank-accounts/src/test/java/danneir/bankaccounts/BankAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package danneir.bankaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
